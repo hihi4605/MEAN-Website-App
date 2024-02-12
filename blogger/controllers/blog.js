bfbe25a079cbbb602ff2188495f1714e212fca88
hihi4605 <christianmichel205@gmail.com>
2024-02-12 23:26:58 +0000

    Real Final

--- blogger/controllers/blog.js
+++ blogger/controllers/blog.js
@@ -1,3 +1,7 @@
+module.exports.blogAdd = function(req, res) {
+    res.render('blogAdd', {title: 'Add Blog'});
+};
+
 module.exports.blogList = function(req, res) {
     res.render('blogList', {title: 'blogList'});
 };
--- blogger/controllers/blogAdd.js
+++
@@ -1,3 +0,0 @@
-module.exports.blogAdd = function(req, res) {
-    res.render('blogAdd', {title: 'Add Blog'});
-};
