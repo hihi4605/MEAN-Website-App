492dc68289fff4aa763b2289dd74d473d6c204c2
csmichel <christianmichel205@gmail.com>
2024-03-20 17:54:26 -0400

    Update routes and views to use kebab-case URLs

--- blogger/app_server/routes/index.js
+++ blogger/app_server/routes/index.js
@@ -5,8 +5,8 @@ var router = express.Router();
 var ctrlBlog = require('../controllers/blog');
 /* Routes to blog and home */
 router.get('/', ctrlBlog.home); 
-router.get('/blogList', ctrlBlog.blogList);
-router.get('/blogAdd', ctrlBlog.blogAdd);
+router.get('/blog-list', ctrlBlog.blogList);
+router.get('/blog-add', ctrlBlog.blogAdd);
 router.get('/blogs/edit/:id', ctrlBlog.blogEdit);
 router.get('/blogs/delete/:id', ctrlBlog.blogDelete);
 
