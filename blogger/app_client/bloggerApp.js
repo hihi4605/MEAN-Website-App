fb20d922a2ede3e23ca2c40d808173b1bcc7fb0d
csmichel <christianmichel205@gmail.com>
2024-04-08 19:20:48 -0400

    Refactor navigation bar style and link colors, and update ng-controller names and homepage layout in index.html and blogList.html

--- blogger/app_client/bloggerApp.js
+++ blogger/app_client/bloggerApp.js
@@ -1,5 +1,5 @@
 var app = angular.module('bloggerApp', ['ui.router']);              
-
+//Router provider
 app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
     $stateProvider
         .state('home', {
@@ -38,49 +38,87 @@ app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', functio
             controller: 'RegisterController',
             controllerAs: 'vm'
         });
-
+    // Default fallback for unmatched urls
     $urlRouterProvider.otherwise('/');
+
     $locationProvider.html5Mode({
         enabled: true,
         requireBase: false
     });
 }]);
 
+//Controller for navigation
+app.controller('NavController', ['$location', 
+    function NavigationController($location){
+        var vm = this;
+        vm.currentPath = $location.path();
+}]);
+
+//Controllers
 app.controller('HomeController', [function() {
     var vm = this;
-    vm.title = 'Christian\'s Blogsite';
+    vm.title = 'Christians Blogsite';
     vm.message = 'Welcome to my blogsite!';
 }]);
 
-app.controller('ListController', ['$http', function($http) {
+
+//*** Controllers ***
+app.controller('ListController', function ListController($http) {
     var vm = this;
     vm.pageHeader = {
-        title: 'Blog List'
+        title: 'Book List'
     };
-    
-    // Fetch all blogs from the API
     getAllBlogs($http)
         .then(function(response) {
-            vm.blogs = response.data;
-        })
-        .catch(function(error) {
-            console.error('Error fetching blogs:', error);
+            vm.books = response.data;
+            vm.message = "Blog data found!";
         });
+});
 
-    // Function to edit a blog
-    vm.editBlog = function(blogId) {
-        console.log('Editing blog with ID:', blogId);
-        // Redirect to the blog edit page
-        // Example: $state.go('blogEdit', { blogid: blogId });
-    };
 
-    // Function to delete a blog
-    vm.deleteBlog = function(blogId) {
-        console.log('Deleting blog with ID:', blogId);
-        // Implement logic to delete the blog
+
+/* Blog Add Controller */
+app.controller('AddController', function BlogAddController($location) {
+    var vm = this;
+    vm.pageHeader = {
+        title: 'Add Blog'
     };
-}]);
+    vm.blog = {};
+    vm.save = function() {
+        addBlog().save(vm.blog, function() {
+            $location.path('/blog-list');
+        });
+    };
+});
+
+/* Blog Edit Controller */
+app.controller('blogEditController', function BlogEditController($location, $routeParams) {
+    var vm = this;
+    vm.pageHeader = {
+        title: 'Edit Blog'
+    };
+    vm.blog = getBlogs().get({ id: $routeParams.id });
+    vm.save = function() {
+        getBlogs().update({ id: $routeParams.id }, vm.blog, function() {
+            $location.path('/blog-list');
+        });
+    };
+});
+
+//*** REST Web API functions ***/
 
 function getAllBlogs($http) {
-    return $http.get('/api/blogs');
+  return $http.get('/api/blogs');
+}
+
+function getBlogbyId($http, id) {
+    return $http.get('/api/blogs/' + id);
+}
+
+function updateBlogById($http, id, data) {
+    return $http.put('/api/blogs/' + id, data);
+}
+
+function addBlog($http, data) {
+    return $http.post('/api/blogs', data);
 }
