2caeb780f154fa3987d471f44d0c6d58849f6c4f
csmichel <christianmichel205@gmail.com>
2024-04-09 17:23:23 -0400

    Refactor app.js to update view engine setup and fix static file paths

--- blogger/app.js
+++ blogger/app.js
@@ -1,53 +1,54 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
+var path = require('path'); 
+
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+const { promiseHooks } = require('v8');
 require('./app_api/models/db');
- 
-
-// API routes
-var routesApi = require('./app_api/routes/index');
-
 var app = express();
 
-// Serve static files before Morgan so these requests aren't logged
-app.use(express.static(path.join(__dirname, 'public')));
+// view engine setup
+app.set('views', path.join(__dirname, '/app_server/views'));
+app.set('view engine', 'ejs');
 
+ 
+var routesApi = require('./app_api/routes/index');
+ 
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'app_client')));
-
-app.use('/js', express.static(__dirname + '/node_modules/angular'));
-app.use('/js', express.static(__dirname + '/node_modules/angular-route'));
-app.use('/js', express.static(__dirname + '/node_modules/angular-ui-router/release'));
-app.use('/js', express.static(__dirname + '/app_client'));
-
-app.use('/api', routesApi);
+app.use(express.static(path.join(__dirname, 'app_client'))); 
  
+app.use('/api',routesApi);
+ 
+// Added per Lab 5 - Angular
 
 app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
+  res.sendFile(path.join(__dirname, 'app_client', 'index.html'))
 });
 
+ 
+     
+console.log('app.js loaded');
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
