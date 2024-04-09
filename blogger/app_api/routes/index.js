7460fcca0d9b9bac8e353b7421676c758fe6978f
csmichel <christianmichel205@gmail.com>
2024-04-09 15:05:43 -0400

    Refactor REST Web API routes in index.js to fix incorrect route path and add home page route

--- blogger/app_api/routes/index.js
+++ blogger/app_api/routes/index.js
@@ -2,23 +2,25 @@ var express = require('express');
 var router = express.Router();
 var ctrlBlog = require('../controllers/blog');
 
-/* Router Pages */
-router.get('/',ctrlBlog.home);
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Blogger' });
+});
 
 /* Return a list of blogs */
-router.get('/api/blogs',ctrlBlog.blogList);
+router.get('/blogs', ctrlBlog.blogList);
 
 /* Return a single blog given an id */
-router.get('api/blogs:id',ctrlBlog.blogList);
+router.get('/blogs/:id',ctrlBlog.blogReadOne);
 
 /* Add a blog */
-router.post('api/blogs/', ctrlBlog.blogAdd);
+router.post('/blogs', ctrlBlog.blogCreate);
 
 /* Update a blog given an id*/
-router.put('api/blogs:id', ctrlBlog.blogEdit);
+router.put('/blogs/:id', ctrlBlog.blogUpdateOne);
 
 /* Delete a blog given an id */
-router.get('api/blogs:id', ctrlBlog.blogDelete);
+router.delete('/blogs/:id', ctrlBlog.blogDelete);
 
 
 module.exports = router;
