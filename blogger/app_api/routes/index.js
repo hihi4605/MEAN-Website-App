73b291c60929616b19fae39449e9f3b628080086
csmichel <christianmichel205@gmail.com>
2024-04-09 15:12:34 -0400

    Fix incorrect route paths and improve organization in REST Web API routes

--- blogger/app_api/routes/index.js
+++ blogger/app_api/routes/index.js
@@ -2,18 +2,13 @@ var express = require('express');
 var router = express.Router();
 var ctrlBlog = require('../controllers/blog');
 
- 
+
 /* Return a list of blogs */
 router.get('/api/blogs',ctrlBlog.blogList);
 
 /* Return a single blog given an id */
 router.get('api/blogs:id',ctrlBlog.blogList);
 
-/* Add a blog */
-router.post('api/blogs/', ctrlBlog.blogAdd);
-
-/* Update a blog given an id*/
-router.put('api/blogs:id', ctrlBlog.blogEdit);
 
 /* Delete a blog given an id */
 router.get('api/blogs:id', ctrlBlog.blogDelete);
