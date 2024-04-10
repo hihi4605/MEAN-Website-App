80e5ee66baf4f9ea71dacbf61f1ef5aaae1e5560
csmichel <christianmichel205@gmail.com>
2024-04-10 02:29:16 -0400

    Refactor blogDeleteOne function in blog.js to use const instead of var and update variable names for clarity

--- blogger/app_api/routes/index.js
+++ blogger/app_api/routes/index.js
@@ -7,6 +7,6 @@ router.get('/blogs', ctrlBlog.blogList);    // GET /api/blogs
 router.post('/blogs', ctrlBlog.blogCreate); // POST /api/blogs
 router.get('/blogs/:blogid', ctrlBlog.blogReadOne); // GET /api/blogs/:blogid
 router.put('/blogs/:blogid', ctrlBlog.blogUpdateOne); // PUT /api/blogs/:blogid
-router.delete('/blogs/:blogid', ctrlBlog.blogDelete);    // DELETE /api/blogs/:blogid
+router.delete('/blogs/:blogid', ctrlBlog.blogDeleteOne);    // DELETE /api/blogs/:blogid
 
 module.exports = router;
